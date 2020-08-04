823c37c5d1784edef63a351c3464c489eaa46443
Rolando.Cintron <rolando.cintron@gmail.com>
2020-08-03 23:55:48 -0400

    feat: starting to write tests

---
+++ test/test.js
@@ -0,0 +1,12 @@
+// const assert = require('chai').assert
+const expect = require("chai").expect;
+
+
+describe("formatNames", function() {
+
+  
+    // it("should return empty string when it gets an empty list", function() {
+    //   expect(formatNames([])).to.equal("");
+    // });
+
+  });
